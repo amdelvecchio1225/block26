eec39646009ea10d65f37083a123fc7620cd3c79
Anthony DelVecchio <amdelvecchio1225@gmail.com>
2023-08-29 14:48:26 -0400

    section 3 complete

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 import ContactRow from './ContactRow';
 
 const dummyContacts = [
@@ -9,6 +10,19 @@ const dummyContacts = [
 
 export default function ContactList() {
     const [contacts, setContacts] = useState(dummyContacts);
+    useEffect(()=>{
+        async function fetchContacts() {
+            try {
+                const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+                const result = await response.json;
+                setContacts(result)
+            }
+            catch(error) {
+                console.error(error);
+            }
+        }
+        fetchContacts();
+    },[]);
     console.log("Contacts: ", contacts);
     return ( 
         <table>
